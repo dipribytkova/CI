95f1bbbbf233ac9173a973595dd1a245d2fa0ea2
Diana Pribytkova <dianababayanarturovna777@gmail.com>
2023-04-14 18:52:47 +0300

    initial commit

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -1,13 +1,12 @@
 package ru.netology.statistic;
 
 public class StatisticsService {
-        public long findMax(long[] incomes) {
-            long currentMax = incomes[0];
-            for (long income : incomes) {
-                if (currentMax < income) {
-                    currentMax = income;
-                }
-            }
-            return currentMax;
-        }
+    public long findMax(long[] incomes) {
+        long current_max_index = 0;
+        long current_max = incomes[0];
+        for (long income : incomes)
+            if (current_max < income)
+                current_max = income;
+        return current_max;
+    }
 }
